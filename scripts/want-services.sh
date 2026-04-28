9933db1e29d6f86c96f764f0edc14ee0e0275928
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2026-04-28 11:18:04 -0500

    kan-sheeeeeeeee

--- scripts/want-services.sh
+++ scripts/want-services.sh
@@ -1,4 +1,5 @@
 set -e
+systemctl --global add-wants niri.service kanshi.service
 systemctl --global add-wants niri.service mako.service
 systemctl --global add-wants niri.service swaybg.service
 systemctl --global add-wants niri.service swayidle.service
