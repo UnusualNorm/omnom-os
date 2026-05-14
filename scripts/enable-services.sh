9c6c6566c156dcda2a3a05c9a76719161ee3a8a8
Unusual Norm <40722525+UnusualNorm@users.noreply.github.com>
2026-05-14 12:33:07 -0500

    download more ram

--- scripts/enable-services.sh
+++ scripts/enable-services.sh
@@ -1,10 +1,12 @@
 set -e
+systemctl enable NetworkManager.service
 systemctl enable bluetooth.service
 systemctl enable chronyd.service
 systemctl enable cups.service
 systemctl enable greetd.service
 systemctl enable libvirtd.service
-systemctl enable NetworkManager.service
 systemctl enable power-profiles-daemon.service
 systemctl enable systemd-oomd.service
+systemctl enable systemd-zram-setup@zram0.service
 systemctl enable zerotier-one.service
+systemctl --global enable syncthing.service
